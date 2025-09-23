efa168b1ab02fb379be1df3bb64bcf36c6658de8
Rusty Conover <rusty@conover.me>
2025-09-23 17:06:57 -0400

    fix: add telemetry

---
+++ src/include/query_farm_telemetry.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include "duckdb.hpp"
+
+#if defined(_WIN32) || defined(_WIN64)
+// Windows: functions are hidden by default unless exported
+#define INTERNAL_FUNC
+#elif defined(__GNUC__) || defined(__clang__)
+// Linux / macOS: hide symbol using visibility attribute
+#define INTERNAL_FUNC __attribute__((visibility("hidden")))
+#else
+#define INTERNAL_FUNC
+#endif
+
+namespace duckdb
+{
+  void QueryFarmSendTelemetry(ExtensionLoader &loader, shared_ptr<DatabaseInstance> db, const string &extension_name, const string &extension_version);
+}
